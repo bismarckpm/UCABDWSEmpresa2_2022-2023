33289770dcaca700c821026a6ca40cc741c0ad78
samuelluist <108497456+samuelluist@users.noreply.github.com>
2022-10-19 12:06:15 -0400

    Cambio clase Estado, tipo ticket, Ticket y Familia Ticket

--- src/backend/ServicesDeskUCABWS/Models/Estado.cs
+++ src/backend/ServicesDeskUCABWS/Models/Estado.cs
@@ -1,15 +1,25 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ServicesDeskUCABWS.Models
 {
     public class Estado
     {
 
+        [Key]
         public Guid Id { get; set; }
+        [Required]
+        [MaxLength(20)]
+        [MinLength(3)]
         public string nombre { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(70)]
+        [MinLength(3)]
         public string descripcion { get; set; } = string.Empty;
+        [Required]
         public DateTime fecha_creacion { get; set; }
+        [Required]
         public DateTime fecha_ultima_edic { get; set; }
-       
+
     }
 }
--- src/backend/ServicesDeskUCABWS/Models/Ticket.cs
+++ src/backend/ServicesDeskUCABWS/Models/Ticket.cs
@@ -1,18 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ServicesDeskUCABWS.Models
 {
     public class Ticket
     {
+        [Key]
         public Guid Id { get; set; }
-        public string nombre { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(60)]
+        [MinLength(3)]
+        public string titulo { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(250)]
+        [MinLength(3)]
         public string descripcion { get; set; } = string.Empty;
+        [Required]
         public DateTime fecha_creacion { get; set; }
+        [Required]
         public DateTime fecha_eliminacion { get; set; }
+        [Required]
         public Estado Estado { get; set; }
+        [Required]
         public Prioridad Prioridad { get; set; }
+        [Required]
         public Tipo_Ticket Tipo_Ticket { get; set; }
+        [Required]
         public HashSet<Votos_Ticket> Votos_Ticket { get; set; }
+        public Familia_Ticket Familia_Ticket { get; set; }
+        public Ticket Ticket_Padre { get; set; }
     }
 }
