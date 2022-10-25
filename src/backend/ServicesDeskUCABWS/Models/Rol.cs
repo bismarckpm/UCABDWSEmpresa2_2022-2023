83aaefcc72a1d7ac996b99367fb41f9fb0ebfde2
CRONOXT <diegocumares@gmail.com>
2022-10-24 21:21:01 -0400

    Get user completado

--- src/backend/ServicesDeskUCABWS/Models/Rol.cs
+++ src/backend/ServicesDeskUCABWS/Models/Rol.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-
+using System.Text.Json;
+using System.Text.Json.Serialization;
 namespace ServicesDeskUCABWS.Models
 {
     public class Rol
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        [System.Text.Json.Serialization.JsonIgnore]
         public ICollection<Usuario> Usuarios { get; set; }
     }
+    public enum JsonIgnoreCondition
+    {
+        Never,
+    };
 }
