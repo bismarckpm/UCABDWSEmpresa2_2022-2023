1f6d8bced33451783cdd4a31a9ac5d6c0e80b999
Alessandro <64756373+AlessBD99@users.noreply.github.com>
2022-11-06 14:36:34 -0400

    Realizado: Estructura MVC y Sidebar funcional

---
+++ src/frontend/ServiceDeskUCAB/Controllers/DepartamentoYGrupoController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceDeskUCAB.Models;
+using ServicesDeskUCABWS.BussinesLogic.DTO.DepartamentoDTO;
+using ServicesDeskUCABWS.BussinesLogic.DTO.GrupoDTO;
+using System.Text.Json;
+
+namespace ServiceDeskUCAB.Controllers
+{
+    public class DepartamentoYGrupoController : Controller
+    {
+        public async Task<IActionResult> DepartamentoGrupo()
+        {
+            
+            try
+            {
+                List<DepartamentoDto> listDepartamentos = new List<DepartamentoDto>();
+                HttpClient client = new HttpClient();
+                var request = new HttpRequestMessage(HttpMethod.Get, "https://localhost:44392/Departamento/ConsultarDepartamento");
+                var _client = await client.SendAsync(request);
+
+                List<GrupoDto> listGrupos = new List<GrupoDto>();
+                HttpClient clientG = new HttpClient();
+                var requestG = new HttpRequestMessage(HttpMethod.Get, "https://localhost:44392/Grupo/ConsultarGrupo");
+                var _clientG = await clientG.SendAsync(requestG);
+
+                if (_client.IsSuccessStatusCode && _clientG.IsSuccessStatusCode)
+                {
+                    var responseStream = await _client.Content.ReadAsStreamAsync();
+                    listDepartamentos = await JsonSerializer.DeserializeAsync<List<DepartamentoDto>>(responseStream);
+
+                    var responseStreamG = await _clientG.Content.ReadAsStreamAsync();
+                    listGrupos = await JsonSerializer.DeserializeAsync<List<GrupoDto>>(responseStreamG);                 
+                    
+                    
+                }
+                var tupla = new Tuple<List<DepartamentoDto>, List<GrupoDto>>(listDepartamentos, listGrupos);
+
+                return View(tupla);              
+
+                
+            }
+            catch (Exception ex)
+            {
+                throw ex.InnerException!;
+            }
+        }
+
+        public async Task<IActionResult> RegistrarDepartamento(DepartamentoDto dept)
+        {
+            try
+            {
+                //cambiar, EL GUID SE GENERARA AUTOMATICO
+                //user.id_direccion = Guid.Parse("9b0ef5e7-d35f-477c-9c16-446b028d70f4");
+                HttpClient client = new HttpClient();
+                var _client = await client.PostAsJsonAsync<DepartamentoDto>("https://localhost:44392/Departamento/CrearDepartamento", dept);
+                return View("AgregarDepartamento");
+            }
+            catch (Exception ex)
+            {
+                throw ex.InnerException!;
+            }
+        }
+
+        [HttpGet]
+        public IActionResult CrearDepartamentoView()
+        {
+            DepartamentoDto departamento = new DepartamentoDto();
+            return PartialView("AgregarDepartamento",departamento);
+        }
+
+
+    }
+    
+}
---
+++ src/frontend/ServiceDeskUCAB/Models/DepartamentoModel.cs
@@ -0,0 +1,16 @@
+﻿using ServicesDeskUCABWS.BussinesLogic.DTO.DepartamentoDTO;
+using ServicesDeskUCABWS.BussinesLogic.DTO.GrupoDTO;
+using System.ComponentModel.DataAnnotations;
+
+namespace ServiceDeskUCAB.Models
+{
+    public class DepartamentoModel
+    {
+        [Required(ErrorMessage = "Introduzca el nombre del departamento")]
+        [StringLength(10, MinimumLength = 5, ErrorMessage = "El nombre debe de tener entre 5 a 10 caracteres")]
+        public string nombre { get; set; }
+
+        [Required(ErrorMessage = "Introduzca contraseña")]
+        public string descripcion { get; set; }
+    }
+}
