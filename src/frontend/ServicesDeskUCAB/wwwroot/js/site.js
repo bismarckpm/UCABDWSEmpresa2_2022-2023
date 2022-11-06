d8f85f74e807179f1b647f2d9d34d78f0ef57d77
Alessandro <64756373+AlessBD99@users.noreply.github.com>
2022-11-06 15:50:41 -0400

    Modificación
    Sidebar funcional y estructura MVC

--- src/frontend/ServicesDeskUCAB/wwwroot/js/site.js
+++ src/frontend/ServicesDeskUCAB/wwwroot/js/site.js
@@ -1,4 +1,16 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your Javascript code.
+// Write your JavaScript code.
+
+$(function (){
+    var PlaceholderElement = $('#Placeholder'); 
+    $('button[data-toggle="modal"]').click(function (event) {
+
+        var url = $(this).data('url');
+        $.get(url).done(function (data){
+            PlaceholderElement.html(data);
+            PlaceholderElement.find('.modal').modal('show');
+        })
+    })
+})
