25fcc1cc1ef29c78975650a7b0bf2edb1ff827d9
Rayner Betancourt <raynerb3000@gmail.com>
2022-11-08 17:01:13 -0400

    Actualizacion del menu bar, agrego de clase prioridasd

---
+++ src/frontend/ServicesDeskUCAB/Models/Prioridad.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ServicesDeskUCAB.Models
+{
+    public class Prioridad
+    {
+        [Required]
+        public int ID { get; set; }
+
+        [Required,MinLength(4),MaxLength(100)]
+        public string Nombre { get; set; }
+
+        [Required, MinLength(4), MaxLength(100)]
+        public string Descripcion { get; set; }
+
+        [Required,MinLength(1),MaxLength(30)]
+        public string Estado { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime FechaDescripcion { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime FechaUltimaEdic { get; set; }
+    }
+}
+
