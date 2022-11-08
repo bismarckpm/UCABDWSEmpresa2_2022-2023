f5141b9eb5426f443a80b463e0266b6f3a16965f
CRONOXT <diegocumares@gmail.com>
2022-11-07 22:29:45 -0400

    Cambios de luis referencias

---
+++ src/frontend/ServicesDeskUCAB/Controllers/UsuarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServicesDeskUCAB.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
