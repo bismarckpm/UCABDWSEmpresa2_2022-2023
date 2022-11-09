de44e89c6b7276f074880e35ee17b41891a640aa
Samuel Luis <sjluis.18@est.ucab.edu.ve>
2022-11-09 09:22:53 -0400

    Agregar y Modificar tipo ticket con pruebas unitarias

---
+++ src/backend/ServicesDeskUCABWS/Prueba.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Hosting;
+using ServicesDeskUCABWS.Data;
+using System.Linq;
+
+namespace ServicesDeskUCABWS
+{
+    class Prueba
+    {
+        DataContext contexto;
+
+        public Prueba()
+        {
+        }
+
+        public Prueba(DataContext contexto)
+        {
+            this.contexto = contexto;
+        }
+        public void Prueba1()
+        {
+            using (var context = new DataContext())
+            {
+                context.Departamentos.ToList();
+            }
+        }
+    }
+    
+}
