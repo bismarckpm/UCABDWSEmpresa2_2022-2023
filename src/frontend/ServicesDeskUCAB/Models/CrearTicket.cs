7eeea1473087a1e0bc80434a2045dab089af155d
Rayner Betancourt <raynerb3000@gmail.com>
2022-11-23 17:03:40 -0400

    acomodo de crear ticket

--- src/frontend/ServicesDeskUCAB/Models/CrearTicket.cs
+++ src/frontend/ServicesDeskUCAB/Models/CrearTicket.cs
@@ -11,7 +11,6 @@ namespace ServicesDeskUCAB.Models
         public Guid prioridad_id { get; set; } 
         public Guid tipoTicket_id { get; set; }
         public Guid departamentoDestino_Id { get; set; }
-        public Guid ticketPadre_Id { get; set; }
     }
 }
 
