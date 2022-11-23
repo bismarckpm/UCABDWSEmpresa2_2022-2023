b3c8bfed32c61e1b1b736247378ebe35293d1496
Gabriel Ojeda <gabrielojeda7@gmail.com>
2022-11-23 01:23:05 -0400

    change default page to login view

--- src/frontend/ServiceDeskUCAB/Program.cs
+++ src/frontend/ServiceDeskUCAB/Program.cs
@@ -35,6 +35,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Login}/{action=Login}/{id?}");
 
 app.Run();
