406388a16dd57e1bf2ef9441fc435bd121e2fe04
CRONOXT <diegocumares@gmail.com>
2022-11-23 21:55:10 -0400

    Update UsuarioServiceTest.cs

--- src/backend/UnitTestServicesDeskUCABWS/UnitTest_GrupoI/UnitTestUsuario/UsuarioServiceTest.cs
+++ src/backend/UnitTestServicesDeskUCABWS/UnitTest_GrupoI/UnitTestUsuario/UsuarioServiceTest.cs
@@ -1,10 +1,13 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using ServicesDeskUCABWS.BussinesLogic.DAO.EtiquetaDAO;
 using ServicesDeskUCABWS.BussinesLogic.DAO.UsuarioDAO;
+using ServicesDeskUCABWS.BussinesLogic.DTO.Usuario;
 using ServicesDeskUCABWS.BussinesLogic.Exceptions;
 using ServicesDeskUCABWS.Data;
+using ServicesDeskUCABWS.Entities;
 using System.Diagnostics;
 using UnitTestServicesDeskUCABWS.UnitTest_GrupoI.DataSeed;
 
@@ -47,11 +50,33 @@ namespace UnitTestServicesDeskUCABWS.UnitTest_GrupoI.UnitTestUsuario
             Assert.ThrowsException<ExceptionsControl>(() => _userService.consularUsuarioID(new Guid("69C30E04-4EB1-4B87-9F32-67DAC2FDC122")));
         }
 
-        [TestMethod(displayName: "Prueba unitaria cuando un usuario coloque una clave o un usuario invalido")]
-        public void LoginExeption()
+
+        [TestMethod(displayName: "Prueba Unitaria intentar eliminar un usuario que no existe")]
+        public void EliminarUsuarioExeptionTest()
+        {
+            var id = new Guid("69C30E04-4EB1-4B87-9F32-67DAC2FDC19B");
+            _contextMock.Setup(p => p.Usuarios).Throws(new Exception(""));
+            Assert.ThrowsException<ExceptionsControl>(() =>  _userService.eliminarUsuario(id));
+
+        }
+
+        [TestMethod(displayName: "Prueba Unitaria Controlador para eliminar un usuario")]
+        public void EliminarPlantillaCtrlExceptionTest()
+        {
+            var id = new Guid("69C30E04-4EB1-4B87-9F32-67DAC2FDC19B");
+            var obj = new UsuarioDto();
+            var result = _userService.eliminarUsuario(id);
+            Assert.IsInstanceOfType(obj, result.GetType());
+
+        }
+
+        /*[TestMethod(displayName: "Prueba Unitaria para logeo satisfactorio")]
+        public void LoginTest()
         {
 
-            //Assert.ThrowsException<ExceptionsControl>(() => _userService.consularUsuarioID();
-        }
+        }*/
+
+
+
     }
 }
