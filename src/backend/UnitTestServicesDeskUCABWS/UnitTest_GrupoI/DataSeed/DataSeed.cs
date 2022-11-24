19de0ce53f98576451f905712474d95514e61c91
Gabriel Ojeda <gabrielojeda7@gmail.com>
2022-11-24 16:52:03 -0400

    Pruebas unitarias Agregar usuarios

--- src/backend/UnitTestServicesDeskUCABWS/UnitTest_GrupoI/DataSeed/DataSeed.cs
+++ src/backend/UnitTestServicesDeskUCABWS/UnitTest_GrupoI/DataSeed/DataSeed.cs
@@ -54,7 +54,22 @@ namespace UnitTestServicesDeskUCABWS.UnitTest_GrupoI.DataSeed
                 Rol = adminRol
             };
 
-
+            var RequestUser = new Usuario
+            {
+                Id = new Guid("69C30E04-4EB1-4B87-9F32-67DAC2FDC19B"),
+                cedula = 12345,
+                primer_nombre = "Gabriel",
+                segundo_nombre = "David",
+                primer_apellido = "Ojeda",
+                segundo_apellido = "Cruz",
+                fecha_nacimiento = "21/12/2020",
+                gender = 'm',
+                correo = "gabrielojeda7@gmail.com",
+                password = "qwertyuiop",
+                fecha_creacion = DateTime.Now.Date,
+                fecha_ultima_edicion = default(DateTime),
+                fecha_eliminacion = default(DateTime),
+            };
 
             var requestListUser = new List<Usuario>
             {
@@ -116,6 +131,10 @@ namespace UnitTestServicesDeskUCABWS.UnitTest_GrupoI.DataSeed
 
             _mockContext.Setup(c => c.Administradores.Add(It.IsAny<Administrador>()));
             _mockContext.Setup(c => c.Administradores.Update(It.IsAny<Administrador>()));
+            _mockContext.Setup(c => c.Usuarios.Add(It.IsAny<Usuario>()));
+            _mockContext.Setup(c => c.RolUsuarios.Add(It.IsAny<RolUsuario>()));
+            _mockContext.Setup(c => c.Empleados.Add(It.IsAny<Empleado>()));
+          
 
             _mockContext.Setup(set => set.DbContext.SaveChanges());
 
--- src/backend/UnitTestServicesDeskUCABWS/UnitTest_GrupoI/UnitTestUsuario/UsuarioServiceTest.cs
+++ src/backend/UnitTestServicesDeskUCABWS/UnitTest_GrupoI/UnitTestUsuario/UsuarioServiceTest.cs
@@ -5,6 +5,7 @@ using ServicesDeskUCABWS.BussinesLogic.DAO.EtiquetaDAO;
 using ServicesDeskUCABWS.BussinesLogic.DAO.UsuarioDAO;
 using ServicesDeskUCABWS.BussinesLogic.Exceptions;
 using ServicesDeskUCABWS.Data;
+using ServicesDeskUCABWS.Entities;
 using System.Diagnostics;
 using UnitTestServicesDeskUCABWS.UnitTest_GrupoI.DataSeed;
 
@@ -47,11 +48,161 @@ namespace UnitTestServicesDeskUCABWS.UnitTest_GrupoI.UnitTestUsuario
             Assert.ThrowsException<ExceptionsControl>(() => _userService.consularUsuarioID(new Guid("69C30E04-4EB1-4B87-9F32-67DAC2FDC122")));
         }
 
-        [TestMethod(displayName: "Prueba unitaria cuando un usuario coloque una clave o un usuario invalido")]
-        public void LoginExeption()
+        [TestMethod(displayName: "Prueba unitaria  para exception en consulta de usuarios")]
+        public void consultaUsuarioException()
+
+        {
+            _contextMock.Setup(p => p.Usuarios).Throws(new Exception());
+
+            Assert.ThrowsException<ExceptionsControl>(() => _userService.ObtenerUsuarios());
+        }
+
+        [TestMethod(displayName: "Prueba unitaria para agregar un Administrador")]
+        public void AgregarAdmin()
+            
+        {
+           
+            var requestUserAdmin = new Usuario
+            {
+                Id = new Guid("69C30E04-4EB1-4B87-9F32-67DAC2FDC19B"),
+                cedula = 12345,
+                primer_nombre = "Gabriel",
+                segundo_nombre = "David",
+                primer_apellido = "Ojeda",
+                segundo_apellido = "Cruz",
+                fecha_nacimiento = "21/12/2020",
+                gender = 'M',
+                correo = "gabrielojeda7@gmail.com",
+                password = "qwertyuiop",
+                fecha_creacion = DateTime.Now.Date,
+                fecha_ultima_edicion = default(DateTime),
+                fecha_eliminacion = default(DateTime),
+            };
+
+            _contextMock.Setup(r => r.Usuarios.Add(requestUserAdmin));
+
+            var UsuarioClient = new RolUsuario
+            {
+                UserId = new Guid("69C30E04-4EB1-4B87-9F32-67DAC2FDC19B"),
+                RolId = new Guid("8C8A156B-7383-4610-8539-30CCF7298162")
+            };
+
+            _contextMock.Setup(r => r.RolUsuarios.Add(UsuarioClient));
+
+            _contextMock.Setup(set => set.DbContext.SaveChanges());
+
+           
+            var usuario = _userService.AgregarAdminstrador(requestUserAdmin);
+
+            Assert.AreEqual(usuario.Id, requestUserAdmin.Id);
+
+        }
+        [TestMethod(displayName: "Prueba unitaria para agregar un Cliente")]
+        public void AgregarCliente()
+
         {
 
-            //Assert.ThrowsException<ExceptionsControl>(() => _userService.consularUsuarioID();
+            var requestUserAdmin = new Usuario
+            {
+                Id = new Guid("69C30E04-4EB1-4B87-9F32-67DAC2FDC19B"),
+                cedula = 12345,
+                primer_nombre = "Gabriel",
+                segundo_nombre = "David",
+                primer_apellido = "Ojeda",
+                segundo_apellido = "Cruz",
+                fecha_nacimiento = "21/12/2020",
+                gender = 'M',
+                correo = "gabrielojeda7@gmail.com",
+                password = "qwertyuiop",
+                fecha_creacion = DateTime.Now.Date,
+                fecha_ultima_edicion = default(DateTime),
+                fecha_eliminacion = default(DateTime),
+            };
+
+            _contextMock.Setup(r => r.Usuarios.Add(requestUserAdmin));
+
+            var UsuarioClient = new RolUsuario
+            {
+                UserId = new Guid("69C30E04-4EB1-4B87-9F32-67DAC2FDC19B"),
+                RolId = new Guid("8C8A156B-7383-4610-8539-30CCF7298161")
+            };
+
+            _contextMock.Setup(r => r.RolUsuarios.Add(UsuarioClient));
+
+            _contextMock.Setup(set => set.DbContext.SaveChanges());
+
+
+            var usuario = _userService.AgregarCliente(requestUserAdmin);
+
+            Assert.AreEqual(usuario.Id, requestUserAdmin.Id);
+
         }
+        [TestMethod(displayName: "Prueba unitaria para agregar un Empleado")]
+        public void AgregarEmpleado()
+
+        {
+
+            var requestUserAdmin = new Usuario
+            {
+                Id = new Guid("69C30E04-4EB1-4B87-9F32-67DAC2FDC19B"),
+                cedula = 12345,
+                primer_nombre = "Gabriel",
+                segundo_nombre = "David",
+                primer_apellido = "Ojeda",
+                segundo_apellido = "Cruz",
+                fecha_nacimiento = "21/12/2020",
+                gender = 'M',
+                correo = "gabrielojeda7@gmail.com",
+                password = "qwertyuiop",
+                fecha_creacion = DateTime.Now.Date,
+                fecha_ultima_edicion = default(DateTime),
+                fecha_eliminacion = default(DateTime),
+            };
+
+            _contextMock.Setup(r => r.Usuarios.Add(requestUserAdmin));
+
+            var UsuarioClient = new RolUsuario
+            {
+                UserId = new Guid("69C30E04-4EB1-4B87-9F32-67DAC2FDC19B"),
+                RolId = new Guid("8C8A156B-7383-4610-8539-30CCF7298163")
+            };
+
+            _contextMock.Setup(r => r.RolUsuarios.Add(UsuarioClient));
+
+            _contextMock.Setup(set => set.DbContext.SaveChanges());
+
+
+            var usuario = _userService.AgregarEmpleado(requestUserAdmin);
+
+            Assert.AreEqual(usuario.Id, requestUserAdmin.Id);
+
+        }
+        [TestMethod(displayName: "Prueba unitaria para actualizar un usuario")]
+        public void ActualizarUsuario()
+        {
+            var requestUserAdmin = new Usuario
+            {
+                Id = new Guid("69C30E04-4EB1-4B87-9F32-67DAC2FDC19B"),
+                cedula = 12345,
+                primer_nombre = "Gabriel",
+                segundo_nombre = "David",
+                primer_apellido = "Ojeda",
+                segundo_apellido = "Cruz",
+                fecha_nacimiento = "21/12/2020",
+                gender = 'M',
+                correo = "gabrielojeda7@gmail.com",
+                password = "qwertyuiop",
+                fecha_creacion = DateTime.Now.Date,
+                fecha_ultima_edicion = default(DateTime),
+                fecha_eliminacion = default(DateTime),
+            };
+
+            _contextMock.Setup(c => c.Usuarios.Add(requestUserAdmin));
+            _contextMock.Setup(set => set.DbContext.SaveChanges());
+            var usuario = _userService.ActualizarUsuario(requestUserAdmin);
+
+            Assert.AreEqual(usuario.correo, requestUserAdmin.correo);
+        }
+
     }
 }
