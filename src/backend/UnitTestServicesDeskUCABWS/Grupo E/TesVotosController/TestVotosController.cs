4d2ec065c9efdb146f598a6a97786e78933c7de7
Samuel Luis <sjluis.18@est.ucab.edu.ve>
2022-11-27 15:55:54 -0400

    TestCtrVotar

---
+++ src/backend/UnitTestServicesDeskUCABWS/Grupo E/TesVotosController/TestVotosController.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using ServicesDeskUCABWS.BussinesLogic.DAO.Tipo_TicketDAO;
+using ServicesDeskUCABWS.BussinesLogic.DAO.Votos_TicketDAO;
+using ServicesDeskUCABWS.Controllers.Tipo_TicketCtr;
+using ServicesDeskUCABWS.Controllers.Votos_TicketCtr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestServicesDeskUCABWS.Grupo_E.TesVotosController
+{
+    public class TestVotosController
+    {
+        private readonly Votos_TicketController _controller;
+        private readonly Mock<IVotos_TicketDAO> _serviceMock;
+
+        public TestVotosController()
+        {
+            _serviceMock = new Mock<IVotos_TicketDAO>();
+            //_controller = new Votos_TicketController(_serviceMock.Object);
+        }
+    }
+}
