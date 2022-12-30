10cf2a3b17b9b3356c96c6d0ba3ec0735e1ac07f
jesusdicen <jesusdesk@gmail.com>
2022-12-30 16:06:07 -0400

    COMMIT DE SEGURIDAD

--- src/backend/ServicesDeskUCABWS/BussinesLogic/DTO/TicketDTO/ActualizarDTO.cs
+++ src/backend/ServicesDeskUCABWS/BussinesLogic/DTO/TicketDTO/ActualizarDTO.cs
@@ -1,15 +1,11 @@
 ﻿using System;
 namespace ServicesDeskUCABWS.BussinesLogic.DTO.TicketDTO
 {
-	public class ActualizarDTO
-	{
-<<<<<<< HEAD
+    public class ActualizarDTO
+    {
         public string estadoId {get; set;}
         public string ticketId {get; set;}
-=======
-        public string estadoId { get; set; }
-        public string ticketId { get; set; }
->>>>>>> 9bd0ee80dff9119112e06e75f6f65c69edc9a8c2
+
     }
 }
 
